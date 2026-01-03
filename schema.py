6e9e7a422cd548a31cd99c10a454c526d8a8df90
chucks <iamdanielchukwuma@gmail.com>
2026-01-03 23:26:23 +0100

    Add Schema and utility file

---
+++ schema.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    # rating: Optional[int] = None 
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+
+    # class Config:
+    #     orm_mode = True
+
+class UserBase(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserCreate(UserBase):
+    pass
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
